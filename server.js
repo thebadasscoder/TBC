406feb0028b82a095d85a1d58c7200fb8699f766
Tionna Wilson Pierce <tionna.wilsonpierce@gmail.com>
2018-10-01 19:04:14 -0400

    Fix webpack-dev-server import and syntax errors.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Webpack = require('webpack');
-const WebpackDevServer = require('../../../lib/Server');
+const WebpackDevServer = require('webpack-dev-server'); 
 const webpackConfig = require('./webpack.config');
 
 
@@ -13,6 +13,6 @@ const devServerOptions = Object.assign({}, webpackConfig.devServer,{
 const server = new WebpackDevServer(compiler, devServerOptions);
 
 
-server.listen(8080, '127.0.0.1', ()=>{
+server.listen(8080, '127.0.0.1', () => {
 	console.log('Starting server on localhost:8080')
-}
+})
